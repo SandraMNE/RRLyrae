9a0d01b2bdaca21fe9c47a944a4d2eb1b4db6a73
sandraMNE <sandra.mitrovic1402@gmail.com>
2014-09-16 04:01:43 +0200

    Added code for: 1. processing files (version of Branimir's code) 2. creating main table 3. plotting based on main table
    Source files (RRLyrae & Others) added as well.

---
+++ plot.py
@@ -0,0 +1,315 @@
+#!/usr/bin/env python
+import numpy as np
+from pyfits import getdata
+from glob import glob
+import numpy as np
+from multiprocessing import Pool
+from scipy.optimize import leastsq
+from numpy.lib import recfunctions
+from useful import extinction_coeff
+import esutil
+from matplotlib import rc
+from matplotlib.backends.backend_pdf import FigureCanvasPdf
+#matplotlib.backend_bases.register_backend('pdf', FigureCanvasPdf)
+import pickle, os
+from dust import getval
+from esutil.coords import eq2gal
+from matplotlib import pyplot as plt
+import os.path
+import math
+import sys, getopt
+
+###########################################################################################################################################
+
+def graph1(chi2_rrlyr, chi2_other):
+
+  # Plots histogram of log(chi_dof) per RRLyrae & Others (non-RRLyrae) 
+  
+  plt.hist(chi2_rrlyr, 30, color = 'b', label='rrlyr', normed=True, alpha=0.5, histtype = 'step')
+  plt.hist(chi2_other, 30, color = 'r', label='other', normed=True, alpha=0.5, histtype = 'step')
+
+  plt.title("Best chi dof for rrlyrae & others")
+  plt.xlim(-1, 2)
+  plt.xlabel("log(chi_dof)")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph1.png")
+
+###########################################################################################################################################
+
+def graph2(period_rrlyr, period_other):
+
+  # Plots histogram of P_best (calculated) per RRLyrae & Others (non-RRLyrae) 
+  
+  plt.hist(period_rrlyr, 30, color = 'b', label='rrlyr', normed=True, alpha=0.5, histtype = 'step')
+  plt.hist(period_other, 30, color = 'r', label='other', normed=True, alpha=0.5, histtype = 'step')
+  plt.title("Best period for rrlyrae & others")
+  plt.xlim(0.3, 0.9)
+  plt.xlabel("period_best")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph2.png")
+
+###########################################################################################################################################
+
+def graph3(period_rrlyr, period_template_rrlyr, period_other, period_template_other):
+
+  # Plots histogram of log(P_best/P_template) per RRLyrae & Others (non-RRLyrae) 
+
+  p_rrlyr = period_rrlyr / period_template_rrlyr
+  p_rrlyr_f = np.array([math.log10(x) for x in p_rrlyr])
+
+  p_other = period_other / period_template_other
+  p_other_f = np.array([math.log10(x) for x in p_other])
+
+  plt.hist(p_rrlyr_f, 30, color = 'b', label='rrlyr', normed=True, alpha=0.5, histtype = 'step')
+  plt.hist(p_other_f, 30, color = 'r', label='other', normed=True, alpha=0.5, histtype = 'step')
+  plt.title("Period diff (best/template) for rrlyrae & others")
+  plt.xlim(-0.15, 0.1)
+  plt.xlabel("log(P_best/P_template)")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph3.png")
+
+###########################################################################################################################################
+
+def graph4(period_rrlyr, period_template_rrlyr, period_other, period_template_other, chi2_rrlyr, chi2_other):
+
+  # Plots scatter plot of log(chi_dof) vs. log(P_best/P_template) per RRLyrae & Others (non-RRLyrae) 
+
+  p_rrlyr = period_rrlyr / period_template_rrlyr
+  p_rrlyr_f = np.array([math.log10(x) for x in p_rrlyr])
+
+  p_other = period_other / period_template_other
+  p_other_f = np.array([math.log10(x) for x in p_other])
+
+  plt.scatter(chi2_rrlyr, p_rrlyr_f, s=10, c='b', marker="+", label='rrlyr')
+  plt.scatter(chi2_other, p_other_f, s=10, c='r', marker="x", label='other')
+
+  plt.title("Chi2_dof vs. Period diff (best/template) for rrlyrae & others")
+  plt.xlim(-0.8, 2.9)
+  plt.xlabel("log(chi2_dof)")
+  plt.ylabel("log(P_best/P_template)")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph4.png")
+
+###########################################################################################################################################
+
+def graph5(period_rrlyr, period_true_rrlyr):
+
+  # Plots scatter plot of P_true vs. log(P_best/P_true) per RRLyrae
+
+  p_rrlyr = period_rrlyr / period_true_rrlyr
+  p_rrlyr_f = np.array([math.log10(x) for x in p_rrlyr])
+
+  plt.scatter(period_true_rrlyr, p_rrlyr_f, s=10, c='b', marker="+", label='rrlyr')
+
+  plt.title("True period vs. Period diff (best/true) for rrlyrae ONLY")
+  plt.xlim(0.15, 1)
+  plt.xlabel("True period")
+  plt.ylabel("log(P_best/P_true)")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph5.png")
+
+###########################################################################################################################################
+
+def graph_ROC(chi2_rrlyr_log, chi2_other_log, dof_rrlyr, dof_other, chi2_rrlyr, chi2_other):
+
+  # Plots ROC for log(chi_dof) and significance
+
+  chi2_range = np.sort(chi2_rrlyr)
+  eff = []
+  comp = []
+  eff_sign = []
+  eff_sign2 = []
+  comp_sign = []
+  comp_sign2 = []
+  control_eff = []
+  control_comp = []
+
+  sigma_rrlyr = np.array([math.sqrt(float(2)/(x-4)) for x in dof_rrlyr])
+  signif_rrlyr = (chi2_rrlyr - 1) / sigma_rrlyr
+
+  sigma_other = np.array([math.sqrt(float(2)/(x-4)) for x in dof_other])
+  signif_other = (chi2_other - 1) / sigma_other
+
+  for T in np.arange(-1.2, 4.2, 0.1):  #chi2_range:
+    num_rrlyr_sel = len(np.where(chi2_rrlyr_log<T)[0])
+    num_other_sel = len(np.where(chi2_other_log<T)[0])
+
+    total_rrlyr = len(chi2_rrlyr_log)
+    if num_rrlyr_sel+num_other_sel != 0:
+      eff_value = float(num_rrlyr_sel)/(num_rrlyr_sel+num_other_sel)
+      eff.append(eff_value)
+      comp_value = float(num_rrlyr_sel)/total_rrlyr
+      comp.append(comp_value)
+
+  for T in np.arange(-5, 100, 0.1):  
+    num_rrlyr_sel_sign = len(np.where(signif_rrlyr<T)[0])
+    num_other_sel_sign = len(np.where(signif_other<T)[0])
+
+    total_rrlyr = len(chi2_rrlyr)
+ 
+    if num_rrlyr_sel_sign+num_other_sel_sign != 0:
+      eff_value_sign = float(num_rrlyr_sel_sign)/(num_rrlyr_sel_sign+num_other_sel_sign)
+      eff_sign.append(eff_value_sign)
+      comp_value_sign = float(num_rrlyr_sel_sign)/total_rrlyr
+      comp_sign.append(comp_value_sign)
+ 
+  for T in [0.0, 0.3, 0.7, 1.0]:
+    num_rrlyr_sel = len(np.where(chi2_rrlyr_log<T)[0])
+    num_other_sel = len(np.where(chi2_other_log<T)[0])
+    total_rrlyr = len(chi2_rrlyr_log)
+    if num_rrlyr_sel+num_other_sel != 0:
+      eff_value = float(num_rrlyr_sel)/(num_rrlyr_sel+num_other_sel)
+      comp_value = float(num_rrlyr_sel)/total_rrlyr
+      control_eff.append(eff_value)
+      control_comp.append(comp_value)
+    
+  comp = np.array(comp)
+  eff = np.array(eff)
+  control_eff = np.array(control_eff)
+  control_comp = np.array(control_comp)
+  plt.plot(comp, eff, lw=1, color='b')
+  plt.plot(comp_sign, eff_sign, color = 'g')
+#  plt.scatter(control_comp, control_eff, color='red', marker='o', s=50)
+
+#  print control_eff, control_comp
+
+  plt.xlabel("Completeness")
+  plt.ylabel("Efficiency")
+  plt.xlim(0,1)
+  plt.ylim(0,1)
+  plt.show()
+  plt.savefig("./graph6-ROC.png")
+
+###########################################################################################################################################
+
+def graph7(chi2_rrlyr, chi2_other, dof_rrlyr, dof_other):
+
+  # Plots histogram of significance per RRLyrae & Others (non-RRLyrae) 
+
+  sigma_rrlyr = np.array([math.sqrt(float(2)/(x-4)) for x in dof_rrlyr])
+  signif_rrlyr = (chi2_rrlyr - 1) / sigma_rrlyr
+
+  sigma_other = np.array([math.sqrt(float(2)/(x-4)) for x in dof_other])
+  signif_other = (chi2_other - 1) / sigma_other
+
+  bins = np.linspace(-10, 100, 55)
+
+  plt.hist(signif_rrlyr, bins, color = 'b', label='rrlyr', normed=True, histtype = 'step')
+  plt.hist(signif_other, bins, color = 'r', label='other', normed=True, histtype = 'step')
+
+  plt.title("Significance for rrlyrae & others")
+  plt.xlim(-10, 100)
+  plt.xlabel("significance")
+  plt.legend(loc='upper right')
+  plt.show()
+  plt.savefig("./graph7.png")  
+
+###########################################################################################################################################
+
+def extract_data(fname):
+
+  # extracts all necessary data for plotting from main table (i.e. file containing it)
+
+  chi2_rrlyr = []
+  t_rrlyr = []
+  p_rrlyr = []
+  dof_rrlyr = []
+  p_true_rrlyr = []
+  p_template_rrlyr = []
+
+
+  chi2_other = []
+  t_other = []
+  p_other = []
+  dof_other = []
+  p_true_other = []
+  p_template_other = []
+
+ 
+  # reading data from file which contains main table
+  main_table = np.genfromtxt(fname, names='Ra, Dec, RRLIndicator, RRLtype, P_true, P_best, chi2dof, N, tID, phi, r0, P_template, rExt, d, uF, gF, rF, iF, zF, ugmin, ugminErr, grmin, grminErr, file # ', usecols=(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23), dtype='f8, f8, i4, |S10, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8, f8', delimiter=",")
+
+  for i in range(len(main_table)): 
+
+    # checking if star is RRLyrae: 1 - yes, 0 - no
+    if main_table["RRLIndicator"][i] == 1:   
+
+      chi2_rrlyr.append(main_table["chi2dof"][i])
+      t_rrlyr.append(main_table["tID"][i])
+      p_rrlyr.append(main_table["P_best"][i])
+      dof_rrlyr.append(main_table["N"][i])
+      p_true_rrlyr.append(main_table["P_true"][i])
+      p_template_rrlyr.append(main_table["P_template"][i])
+    
+    elif main_table["RRLIndicator"][i] == 0:
+
+      chi2_other.append(main_table["chi2dof"][i])
+      t_other.append(main_table["tID"][i])
+      p_other.append(main_table["P_best"][i])
+      dof_other.append(main_table["N"][i])
+      p_true_other.append(main_table["P_true"][i])
+      p_template_other.append(main_table["P_template"][i])
+      
+  return np.array(chi2_rrlyr), np.array(t_rrlyr), np.array(p_rrlyr), np.array(dof_rrlyr), np.array(p_true_rrlyr), np.array(p_template_rrlyr), np.array(chi2_other), np.array(t_other), np.array(p_other), np.array(dof_other), np.array(p_true_other), np.array(p_template_other)
+
+###########################################################################################################################################
+
+#### main program #####
+
+if __name__ == "__main__":   
+
+#  data_path = "/homec/sfb881/sfb044/projects/rrlyr/PS_SANDRA/"
+#  file_name = "main_table.txt"
+#  data = data_path + file_name
+
+
+  # checking if input file is supplied as argument
+  if len(sys.argv) > 1:
+
+    try:
+      opts, args = getopt.getopt(sys.argv[1:],"hi:",["ifile="])
+
+    except getopt.GetoptError:
+      print "python plot.py -i <inputfile>"
+      sys.exit(2)
+
+    for opt, arg in opts:
+      if opt == "-h":
+        print "python plot.py -i <inputfile>"
+        sys.exit(2)
+      elif opt in ("-i", "--ifile"):
+        data = arg
+
+    # these flags are currently useless, but might be useful if C templates are added or F is fitted too
+    Cflag = 0
+    Fflag = 0
+  
+    if Cflag == 0 and Fflag == 0: 
+
+      # retrieve data
+      chi2_dof_rrlyr, template_rrlyr, period_rrlyr, dof_rrlyr, period_true_rrlyr, period_template_rrlyr, chi2_dof_other, template_other, period_other, dof_other, period_true_other, period_template_other = extract_data(data)
+
+
+    # calculate log(chi2) for plotting
+    chi2_dof_rrlyr_log = np.array([math.log10(x) for x in chi2_dof_rrlyr])
+    chi2_dof_other_log = np.array([math.log10(x) for x in chi2_dof_other])
+
+    # plot
+
+    graph1(chi2_dof_rrlyr_log, chi2_dof_other_log)
+    graph2(period_rrlyr, period_other)
+    graph3(period_rrlyr, period_template_rrlyr, period_other, period_template_other)
+    graph4(period_rrlyr, period_template_rrlyr, period_other, period_template_other,chi2_dof_rrlyr_log, chi2_dof_other_log)
+    graph5(period_rrlyr, period_true_rrlyr)
+    graph_ROC(chi2_dof_rrlyr_log, chi2_dof_other_log, dof_rrlyr, dof_other, chi2_dof_rrlyr, chi2_dof_other)
+    graph7(chi2_dof_rrlyr, chi2_dof_other, dof_rrlyr, dof_other)
+
+  else:
+
+    print "Missing argument! Correct usage: python plot.py -i <inputfile full path>"
+
---
+++ prepare_for_plotting.py
@@ -0,0 +1,241 @@
+#!/usr/bin/env python
+import numpy as np
+from pyfits import getdata
+from glob import glob
+import numpy as np
+from multiprocessing import Pool
+from scipy.optimize import leastsq
+from numpy.lib import recfunctions
+from useful import extinction_coeff
+import esutil
+from matplotlib import rc
+from matplotlib.backends.backend_pdf import FigureCanvasPdf
+#matplotlib.backend_bases.register_backend('pdf', FigureCanvasPdf)
+import pickle, os
+from dust import getval
+from esutil.coords import eq2gal
+from matplotlib import pyplot as plt
+import os.path
+import cPickle
+from esutil.coords import sphdist
+import sys, getopt
+
+##############################################################################################################################################
+
+def extract_data(pkl_fname):
+
+    # extract all relevant data from processed pkl files
+
+    f = open(pkl_fname)
+    res_all = pickle.load(f)
+    f.close()
+
+    templateID = []
+    chi2 = []
+    period = []
+    phi = []
+    r = []
+    dof = []
+    F = []
+
+    for res in res_all:
+        templateID.append(res[0])
+        chi2.append(res[1])  
+        period.append(res[2])  
+        phi.append(res[3][0])
+        r.append(res[3][1])  
+#        F.append(res[3][2])  # currently not available
+        dof.append(res[4])
+
+    period = np.array(period)
+    templateID = np.array(templateID)
+    chi2 = np.array(chi2)
+    phi = np.array(phi)
+    r = np.array(r)
+    dof = np.array(dof)
+    f = np.array(F)
+
+    return period, templateID, chi2, dof, phi, r, F
+
+##############################################################################################################################################
+
+def load_PS1_data(lc_filename):
+    # load cleaned PS1 data and correct magnitude errors
+    lc_ps = np.genfromtxt(lc_filename, usecols=(1, 2, 3, 6, 8, 17), names='mag_err, ra, dec, filter, mjd_obs, ucalmag', dtype='f8, f8, f8, |S5, f8, f8')
+    lc_ps['mag_err'] = np.sqrt((1.3*lc_ps['mag_err'])**2 + 0.015**2)
+    # correct PS1 light curves for extinction
+    gl, gb = eq2gal(lc_ps['ra'][0], lc_ps['dec'][0])
+    EBV = getval(gl, gb)
+    for band, c in zip(['g', 'r', 'i', 'z', 'y'], [3.172, 2.271, 1.682, 1.322, 1.087]):
+        lc_ps['ucalmag'][lc_ps['filter'] == band] -= c*EBV
+    # light curve data used in fitting
+    lc_data = [lc_ps['mjd_obs'][lc_ps['filter'] == 'g'], lc_ps['ucalmag'][lc_ps['filter'] == 'g'], lc_ps['mag_err'][lc_ps['filter'] == 'g'], lc_ps['mjd_obs'][lc_ps['filter'] == 'r'], lc_ps['ucalmag'][lc_ps['filter'] == 'r'], lc_ps['mag_err'][lc_ps['filter'] == 'r'], lc_ps['mjd_obs'][lc_ps['filter'] == 'i'], lc_ps['ucalmag'][lc_ps['filter'] == 'i'], lc_ps['mag_err'][lc_ps['filter'] == 'i'], lc_ps['mjd_obs'][lc_ps['filter'] == 'z'], lc_ps['ucalmag'][lc_ps['filter'] == 'z'], lc_ps['mag_err'][lc_ps['filter'] == 'z']]
+    return lc_data
+
+##############################################################################################################################################
+
+#### main program #####
+
+if __name__ == "__main__":  
+  
+  #suffix = "_withN"
+  #data_other = "/homec/sfb881/sfb044/projects/rrlyr/PS_SANDRA/processed/lc_cleaned_other/*" + suffix + ".pkl"
+  #data_rrlyr = "/homec/sfb881/sfb044/projects/rrlyr/PS_SANDRA/processed/lc_cleaned_rrlyr/*" + suffix + ".pkl"
+  #file_name = "main_table.txt"
+  #home_data = '/homec/sfb881/sfb044/papers/halo_sub'
+
+  if len(sys.argv)>1:
+  
+    # check the arguments 
+    try:
+      opts, args = getopt.getopt(sys.argv[1:],"hs:r:n:t:o:",["suffix=","rrlyr_path=","non-rrlyr_path=","tables_path=","output_file="])
+
+    except getopt.GetoptError:
+   
+      print "python prepare_for_plotting.py -s <pkl file suffix> -r <rrlyr full path> -n <non-rrlyr full path> -t <tables (true params) full path> -o <output file full path>"
+      sys.exit(2)
+  
+    for opt, arg in opts:
+      if opt == "-h":
+        print "python prepare_for_plotting.py -s <pkl file suffix> -r <rrlyr full path> -n <non-rrlyr full path> -t <tables (true params) full path> -o <output file full path>"
+        sys.exit(2)
+      elif opt in ("-s", "--suffix"):
+        suffix = arg
+      elif opt in ("-r", "--rrlyr_path"):
+        rrlyr_path = arg
+      elif opt in ("-n", "--non-rrlyr_path"):
+        other_path = arg
+      elif opt in ("-t", "--tables_path"):
+        home_path = arg
+      elif opt in ("-o", "--output_file"):
+        file_name = arg
+
+    data_rrlyr = rrlyr_path + "/*" + suffix + ".pkl"
+    data_other = other_path + "/*" + suffix + ".pkl"
+  
+    #making list of all pickle files to be processed
+    fnames_o = np.sort(glob(data_other))
+    fnames_r = np.sort(glob(data_rrlyr))
+
+    fnames = np.concatenate((fnames_r, fnames_o))
+
+
+    names_other = []
+    chi2_best_other = []
+    period_best_other = []
+    template_best_other = []
+    dof_best_other = []
+    phi_best_other = []
+    r0_best_other = []
+#    F_best_other = []
+
+
+    names_rrlyr = []
+    chi2_best_rrlyr = []
+    period_best_rrlyr = []
+    template_best_rrlyr = []
+    dof_best_rrlyr = []
+    phi_best_rrlyr = []
+    r0_best_rrlyr = []
+#    F_best_rrlyr = []
+
+
+    table2 = np.genfromtxt('%s/table2.dat' % home_path, names='id, type, P, rA', usecols=(0, 1, 2, 11), dtype='u4, |S10, f8, f8')
+
+    # load light curve parameters of SDSS S82 RR Lyrae stars
+    true_params = np.genfromtxt('%s/table2.dat' % home_path, names='id, type, P, gA, g0, gE, gT, rA, r0, rE, rT, iA, i0, iE, iT, zA, z0, zE, zT', usecols=(0, 1, 2, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22), dtype='u4, |S10, f8, f8, f8, f8, u2, f8, f8, f8, u2, f8, f8, f8, u2, f8, f8, f8, u2')
+
+    # select only RRab stars
+    ab = true_params['type'] == 'ab'
+    # currently we do not use c templates
+    # select 2 type c stars
+    # c = (true_params['id'] == 1342595) | (true_params['id'] == 3031571)
+    # true_params = true_params[ab | c]
+    true_params = true_params[ab]
+
+    # load from table3 ra & dec
+    table3 = np.genfromtxt('%s/table3.dat' % home_path, usecols=(0,1,2,3,4,6,7,8,9,10,12,13,14,15), names='id, ra, dec, rExt, d, uF, gF, rF, iF, zF, ugmin, ugminErr, grmin, grminErr')
+  
+    f_tbl = open(file_name,"w")
+
+    f_tbl.write("#Ra, Dec, RRLIndicator, RRLtype, P_true, P_best, chi2dof, N, tID, phi, r0, P_template, rExt, d, uF, gF, rF, iF, zF, ugmin, ugminErr, grmin, grminErr, file #  \n")
+
+
+    c = 0
+
+    # calculating values for each star
+    for f in fnames:
+
+      path = f[0:f.rfind("/")]
+
+      txt_f = f.replace(".pkl",".txt").replace(suffix,"")
+
+      RaDec = np.genfromtxt(txt_f, usecols=(2, 3), names='ra, dec', dtype='f8, f8')[0]
+      ra = RaDec[0]
+      dec = RaDec[1]
+      starID = f[f.rfind("/")+1:].replace("ps1_-","").replace("cleaned","").replace(suffix,"")
+  
+      c = c + 1
+      print "Currently processing", c, " out of ", len(fnames)
+
+      # loading data from pkl file
+      period, template, chi2, dof, phi, r0, F = extract_data(f)
+
+      # finding best of everything (for min chi2)
+      p = period[np.argmin(chi2)]
+      tID = template[np.argmin(chi2)]
+      N = dof[np.argmin(chi2)]
+      ch2 = chi2[np.argmin(chi2)]
+      chi2dof = ch2/(N-4)
+      phi_b = phi[np.argmin(chi2)]
+      r0_b = r0[np.argmin(chi2)]
+
+      if path in data_rrlyr:
+ 
+        # if star is RRLyrae
+        RRLInd = 1
+        P_template = true_params['P'][tID]
+        ang_sep = sphdist(ra, dec, table3['ra'], table3['dec'])*3600
+        ind = np.where(ang_sep < 1)[0]
+        rExt = table3['rExt'][ind][0]
+        d = table3['d'][ind][0]
+        uF = table3['uF'][ind][0]
+        gF = table3['gF'][ind][0]
+        rF = table3['rF'][ind][0] 
+        iF = table3['iF'][ind][0] 
+        zF = table3['zF'][ind][0]
+        ugmin = table3['ugmin'][ind][0]
+        ugminErr = table3['ugminErr'][ind][0]
+        grmin = table3['grmin'][ind][0]
+        grminErr = table3['grminErr'][ind][0]    
+
+        if ind != None:
+          p_true = table2['P'][ind][0]
+          typ = table2['type'][ind][0]
+
+      else:
+
+        RRLInd = 0
+        P_template = true_params['P'][tID]
+        p_true = -99999.99
+        typ = None
+        rExt = -99999.99
+        d = -99999.99
+        uF = -99999.99
+        gF = -99999.99
+        rF = -99999.99
+        iF = -99999.99
+        zF = -99999.99
+        ugmin = -99999.99
+        ugminErr = -99999.99
+        grmin = -99999.99
+        grminErr = -99999.99 
+
+     
+      f_tbl.write(str(ra)+","+str(dec)+","+str(RRLInd)+","+str(typ)+","+str(p_true)+","+str(p)+","+str(chi2dof)+","+str(N)+","+str(tID)+","+str(phi_b)+","+str(r0_b)+","+str(P_template)+","+str(rExt)+","+str(d)+","+str(uF)+","+str(gF)+","+str(rF)+","+str(iF)+","+str(zF)+","+str(ugmin)+","+str(ugminErr)+","+str(grmin)+","+str(grminErr)+","+str(starID)+"\n")
+
+    f_tbl.close()  
+
+  else:
+
+    print "Incorrect number of arguments! Correct usage:\n print "python prepare_for_plotting.py -s <pkl file suffix> -r <rrlyr full path> -n <non-rrlyr full path> -t <tables (true params) full >"
